86efafd5a5ca650b32f2c9ea456712fac4cd6f0b
unknown <pavithrasivanoli@gmail.com>
2022-12-16 22:43:45 +0530

    updated the files

---
+++ js/new.js
@@ -0,0 +1,29 @@
+//function with argument and with return type
+
+/*function pavi(a,b){
+	return a+b;
+}
+console.log(pavi(2,6))*/
+
+//function with argument and with without return type
+
+/*
+/function pavi(a,b){
+	console.log(a+b);
+}
+pavi(2,6)*/
+
+//function without argument and with  return type
+
+/*function pavi(){
+	return '1';
+}
+d=pavi()
+console.log(d)*/
+
+
+//function without argument and without return type
+/*function pavi(){
+	console.log("hello");
+}
+pavi()*/
